cd8d74a050be0d41da03a3f0294aa08fd692263b
Olivia <oliviakim217@gmail.com>
2023-08-29 16:07:51 -0400

    Update api key and readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # air-quality-alert
 Receive a notification when the air quality in Toronto becomes poor
+
+## How To Start
+
+To start the server, use the following commands.
+```javascript
+// Install all modules
+npm i
+
+// Keep the server running
+nodemon index.js
+
+```
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express from "express";
 import axios from "axios";
+import 'dotenv/config';
  
 const app = express();
 const port = 3000;
-const apiKey = "ed802ebf-f249-4ad7-85ae-1d894e8e3b51";
 
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
     // Fetch air pollution data from Air Visual
     try{
-        const result = await axios.get(`http://api.airvisual.com/v2/city?city=Toronto&state=Ontario&country=Canada&key=${apiKey}`);
+        const result = await axios.get(`http://api.airvisual.com/v2/city?city=Toronto&state=Ontario&country=Canada&key=${process.env.API_KEY}`);
         const aqi = result.data.data.current.pollution.aqius;
         const mainPollutant = result.data.data.current.pollution.mainus;
         const timeStamp = result.data.data.current.pollution.ts.slice(0, 10);
